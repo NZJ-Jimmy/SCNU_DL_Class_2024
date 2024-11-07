054febbcce82b274d5e6f9b754f610be9c91fe31
Jimmy Leung <JimmyLeung@m.scnu.edu.cn>
2024-11-07 23:45:55 +0800

    hw07 q1

--- hw07_Hamlet/nlp_bigram_ppl_TODO.py
+++ hw07_Hamlet/nlp_bigram_ppl_TODO.py
@@ -14,7 +14,7 @@ debug = False
 ###############################################
 # generate Bi-Gram counter for training corpus
 ###############################################
-corpus_text = '''I play tennis. I like Chinese friends. I play tennis with Chinese friends. I have friends who like tennis.'''
+corpus_text = '''I play tennis. I like Chinese friends. I talk with Chinese student. I play with tennis friends. I have friends who like tennis.'''
 token = nltk.word_tokenize(corpus_text)
 
 ###########################
@@ -45,3 +45,17 @@ if debug:
 # TODO: lookup each query bigram in each query_text
 # compute Uni-Counter[bg[0]] /  Bi-Counter[(bg[0],bg[1])]
 # convert to PPL and output
+
+# P(w2|w1) = P(w1,w2) / P(w1) = Bi-Counter[(w1,w2)] / Uni-Counter[w1]
+
+# PPL   = (1 / (P(w2|w1) * P(w3|w2) * ... * P(wn|wn-1))) ** (1/n)
+#       = ((Uni-Counter[w1] / Bi-Counter[(w1,w2)]) * (Uni-Counter[w2] / Bi-Counter[(w2,w3)]) * ... * (Uni-Counter[wn-1] / Bi-Counter[(wn-1,wn)])) ** (1/n)
+
+prod = 1    # product of (Uni-Counter / Bi-Counter)
+
+for bigram in query_bigram:
+    prod *= unigrams[bigram[0]] / bigrams[bigram]
+    
+ppl = prod ** (1/len(query_bigram))
+
+print(ppl)
